29c702fc3fc6661365a80f5a608ac38dcd1f3886
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2024-01-11 23:38:04 +0000

    fix_v2

--- sorting_algos/bubble_sort.js
+++ sorting_algos/bubble_sort.js
@@ -5,7 +5,7 @@ function bubbleSort(arr) {
     for (let i = 0; i < n; i++) { // Outer loop: iterate over the array.
         flag = false; // Assume the array is sorted until a swap is made.
   
-        for (let j = 0; j < n - i; j++) { //to prevent comparing with the sorted elements at the right
+        for (let j = 0; j < n - i; j++) { //! we use i form the outer loop to prevent comparing with the sorted elements at the right 
             if (arr[j] > arr[j + 1]) { // Compare adjacent elements.
                 // Swap if the current element is greater than the next element.
                 let temp = arr[j];
